e4280ba752b707196051bd6a60b814e254d01d74
dargy <dragonismbot@protonmail.com>
2024-01-10 14:53:40 -0600

    Revert "chore: clean up (add videoRoutes.ts)"
    This reverts commit 3fd4a73d80328bee92f0f13ff7a6d2ad637b21e5.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
 import { Hono, Handler } from 'hono'
 import { cache } from 'hono/cache'
+import { HandlerResponse } from 'hono/types'
 
-import { getVideoInfo } from '@/services/tiktok'
-import generateAlternate from '@/util/generateAlternate'
-import { handleVideo} from '@/videoRoutes'
+import { grabAwemeId, getVideoInfo } from './services/tiktok'
+import { VideoResponse, ErrorResponse } from './templates'
+import generateAlternate from './util/generateAlternate'
 
 const app = new Hono()
+
+app.get('/test/:videoId', async (c) => {
+    const { videoId } = c.req.param()
+    const awemeId = await getVideoInfo(videoId)
+    
+    if(awemeId instanceof Error) {
+        return new Response((awemeId as Error).message, { status: 500 })
+    }
+
+    return new Response(JSON.stringify(awemeId), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+        }
+    })
+})
 
 app.get('/', (c) => {
     return new Response('', {
@@ -16,32 +33,59 @@ app.get('/', (c) => {
     })
 })
 
-const routes = [
-    {
-        path: '/:videoId',
-        handler: handleVideo
-    },
-    {
-        path: '/*/video/:videoId',
-        handler: handleVideo
-    },
-    {
-        path: '/t/:videoId',
-        handler: handleVideo
-    }
-]
-    
-// temp-fix: add trailing slash to all routes
-routes.forEach(route => {
-    app.get(route.path, route.handler)
-    app.get(route.path + '/', route.handler)
-})
+const returnHTMLResponse = (content: string, status?: number): HandlerResponse<Response> => {
+    return new Response(content, {
+        status: status || 200,
+        headers: {
+            'Content-Type': 'text/html; charset=utf-8',
+            'Cache-Control': 'public, max-age=3600'
+        }
+    })
+}
 
-// Cache all generate requests for 1 hour
-app.get('/generate/*', cache({
-    cacheName: 'my-app',
-    cacheControl: 'max-age=3600',
-}))
+async function handleVideo(c: any): Promise<Response> {
+    const awemeIdPattern = /^\d{1,19}$/;
+    const BOT_REGEX = /bot|facebook|embed|got|firefox\/92|curl|wget|go-http|yahoo|generator|whatsapp|discord|preview|link|proxy|vkshare|images|analyzer|index|crawl|spider|python|cfnetwork|node/gi
+
+    const { videoId } = c.req.param()
+    let id = videoId;
+
+    // If the user agent is a bot, redirect to the TikTok page
+    if (!BOT_REGEX.test(c.req.header('User-Agent') || '')) {
+        return new Response('', {
+            status: 302,
+            headers: {
+                'Location': 'https://www.tiktok.com' + `${awemeIdPattern.test(videoId) ? c.req.path : '/t/' + videoId}`
+            }
+        })
+    }
+
+    // If the videoId needs to be validated, do it here
+    if (!awemeIdPattern.test(videoId)) {
+        try {
+            const awemeId = await grabAwemeId(videoId)
+            id = awemeId
+        } catch(e) {
+            const responseContent = await ErrorResponse((e as Error).message);
+            return returnHTMLResponse(responseContent, 201) as Response;
+        }
+    }
+
+    try {
+        const videoInfo = await getVideoInfo(id)
+
+        if (videoInfo instanceof Error) {
+            const responseContent = await ErrorResponse((videoInfo as Error).message);
+            return returnHTMLResponse(responseContent, 201) as Response;
+        }
+
+        const responseContent = await VideoResponse(videoInfo);
+        return returnHTMLResponse(responseContent) as Response;
+    } catch(e) {
+        const responseContent = await ErrorResponse((e as Error).message);
+        return returnHTMLResponse(responseContent, 201) as Response;
+    }
+}
 
 app.get('/generate/alternate', (c) => {
     const content = JSON.stringify(generateAlternate(c));
@@ -53,6 +97,19 @@ app.get('/generate/alternate', (c) => {
         }
     })
 })
+
+function getExpiry(url: URL) {
+    const hex = new URL(url).pathname.split('/')[2];
+    return new Date(parseInt(hex, 16) * 1000);
+}
+
+app.get(
+    '/generate/*',
+    cache({
+      cacheName: 'my-app',
+      cacheControl: 'max-age=3600',
+    })
+  )
 
 app.get('/generate/video/:videoId', async (c) => {
     const { videoId } = c.req.param()
@@ -90,6 +147,27 @@ app.get('/generate/image/:videoId', async (c) => {
     } else {
         return new Response(JSON.stringify(data), { status: 200 })
     }
+})
+
+const routes = [
+    {
+        path: '/:videoId',
+        handler: handleVideo
+    },
+    {
+        path: '/*/video/:videoId',
+        handler: handleVideo
+    },
+    {
+        path: '/t/:videoId',
+        handler: handleVideo
+    }
+]
+
+// temp-fix: add trailing slash to all routes
+routes.forEach(route => {
+    app.get(route.path, route.handler)
+    app.get(route.path + '/', route.handler)
 })
 
 export default app
