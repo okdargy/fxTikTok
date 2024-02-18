4fcfb031c530c1b0b15d09920798e9027d407556
dargy <dragonismbot@protonmail.com>
2024-02-18 01:44:56 -0600

    minor: uppercase M & B for formatNumber

--- src/util/generateAlternate.tsx
+++ src/util/generateAlternate.tsx
@@ -10,8 +10,8 @@ function formatNumber(value: string): string {
     if (num < 1000) return num.toString()
     if (num < 10000) return (num / 1000).toFixed(1) + 'k'
     if (num < 1000000) return (num / 1000).toFixed(1) + 'k'
-    if (num < 10000000) return (num / 1000000).toFixed(1) + 'm'
-    if (num < 1000000000) return (num / 1000000).toFixed(1) + 'm'
+    if (num < 10000000) return (num / 1000000).toFixed(1) + 'M'
+    if (num < 1000000000) return (num / 1000000).toFixed(1) + 'M'
     if (num < 10000000000) return (num / 1000000000).toFixed(1) + 'b'
     return (num / 1000000000).toFixed(0) + 'b'
 }
